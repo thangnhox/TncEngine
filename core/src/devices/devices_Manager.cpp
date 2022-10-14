5e1963a959a011a0b4a85f3c3687f668c0936578
Ton Duc Thang <tncthng347@gmail.com>
2022-10-14 14:11:36 +0700

    complete copyBufferToImage

--- core/src/devices/devices_Manager.cpp
+++ core/src/devices/devices_Manager.cpp
@@ -513,7 +513,20 @@ namespace vulkanEngine { namespace devices {
         VkCommandBuffer commandBuffer = beginSingleTimeCommands();
 
         VkBufferImageCopy region{};
-        
+        region.bufferOffset = 0;
+        region.bufferRowLength = 0;
+        region.bufferImageHeight = 0;
+
+        region.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        region.imageSubresource.mipLevel = 0;
+        region.imageSubresource.baseArrayLayer = 0;
+        region.imageSubresource.layerCount = layerCount;
+
+        region.imageOffset = {0, 0, 0};
+        region.imageExtent = {width, height, 1};
+
+        vkCmdCopyBufferToImage(commandBuffer, buffer, image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &region);
+        endSingleTimeCommands(commandBuffer);
     }
 
 }}
