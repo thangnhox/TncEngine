723abd082ae765553dd885514239153dca86e349
Ton Duc Thang <tncthng347@gmail.com>
2023-06-01 22:38:33 +0700

    Add glm

--- Makefile
+++ Makefile
@@ -22,6 +22,7 @@ GLFW_INCLUDE = -Icore/vendore/GLFW/include
 GLAD_INCLUDE = -Icore/vendore/Glad/include
 IMGUI_FRONTENDS_INCLUDE = -Icore/vendore/ImGUI/frontends
 IMGUI_BACKENDS_INCLUDE = -Icore/vendore/ImGUI/backends
+GLM_INCLUDE = -Icore/vendore/glm
 
 # My include
 CORE_INCLUDE = -Icore/src
@@ -40,7 +41,7 @@ setup:
 	cd bin/submoduleBuild/GLFW && $(MAKE) && cp src/libglfw3.a ../../lib/
 
 core:
-	g++ $(LIB_BUILD) $(CPPFLAGS) $(SPDLOG_INCLUDE) $(GLAD_INCLUDE) $(GLFW_INCLUDE) $(CORE_INCLUDE) $(IMGUI_FRONTENDS_INCLUDE) $(IMGUI_BACKENDS_INCLUDE) $(TNC_DEBUG) -c $(shell find core/src/ -name '*.cpp')
+	g++ $(LIB_BUILD) $(CPPFLAGS) $(SPDLOG_INCLUDE) $(GLAD_INCLUDE) $(GLFW_INCLUDE) $(CORE_INCLUDE) $(IMGUI_FRONTENDS_INCLUDE) $(IMGUI_BACKENDS_INCLUDE) $(GLM_INCLUDE) $(TNC_DEBUG) -c $(shell find core/src/ -name '*.cpp')
 	mv *.o bin/objectFiles/core/
 	ar src bin/lib/libTncEngineCore.a $(shell find bin/objectFiles/core/ -name '*.o')
 
@@ -58,4 +59,4 @@ imgui:
 	ar src bin/lib/libimgui.a $(shell find bin/objectFiles/imgui/ -name '*.o')
 
 sandbox:
-	g++ $(CPPFLAGS) -Lbin/lib $(CORE_INCLUDE) $(SPDLOG_INCLUDE) $(GLAD_INCLUDE) $(GLFW_INCLUDE) $(IMGUI_FRONTENDS_INCLUDE) $(IMGUI_BACKENDS_INCLUDE) -o bin/intermidiate/SandboxApp $(shell find sandbox/src/ -name '*.cpp') $(CORE_FLAG) $(GLFW_FLAG) $(GLAD_FLAG) $(GL_FLAG) $(IMGUI_FLAG)
+	g++ $(CPPFLAGS) -Lbin/lib $(CORE_INCLUDE) $(SPDLOG_INCLUDE) $(GLAD_INCLUDE) $(GLFW_INCLUDE) $(IMGUI_FRONTENDS_INCLUDE) $(IMGUI_BACKENDS_INCLUDE) $(GLM_INCLUDE) -o bin/intermidiate/SandboxApp $(shell find sandbox/src/ -name '*.cpp') $(CORE_FLAG) $(GLFW_FLAG) $(GLAD_FLAG) $(GL_FLAG) $(IMGUI_FLAG)
--- sandbox/src/SanboxApp.cpp
+++ sandbox/src/SanboxApp.cpp
@@ -1,11 +1,26 @@
 #include <TncEngine.hpp>
 
+#include <glm/vec3.hpp>                 // glm::vec3
+#include <glm/vec4.hpp>                 // glm::vec4
+#include <glm/mat4x4.hpp>               // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+glm::mat4 camera(float Translate, glm::vec2 const &Rotate)
+{
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.f);
+    glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+    View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+    View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+    return Projection * View * Model;
+}
+
 class ExampleLayer : public TncEngine::Layer
 {
 public:
     ExampleLayer()
         : Layer("Example")
-    {}
+    {
+    }
 
     void OnUpdate() override
     {
@@ -13,11 +28,11 @@ public:
             TncEngine_TRACE("Tab key is pressed! (poll)");
     }
 
-    void OnEvent(TncEngine::Event& event) override
+    void OnEvent(TncEngine::Event &event) override
     {
         if (event.GetEventType() == TncEngine::EventType::KeyPressed)
         {
-            TncEngine::KeyPressedEvent& e = (TncEngine::KeyPressedEvent&)event;
+            TncEngine::KeyPressedEvent &e = (TncEngine::KeyPressedEvent &)event;
             if (e.GetKeyCode() == TNC_KEY_TAB)
                 TncEngine_TRACE("Tab key is pressed! (event)");
             TncEngine_TRACE("{0}", (char)e.GetKeyCode());
@@ -39,7 +54,7 @@ public:
     }
 };
 
-TncEngine::Application* TncEngine::CreateApplication()
+TncEngine::Application *TncEngine::CreateApplication()
 {
     return new Sanbox();
 }
