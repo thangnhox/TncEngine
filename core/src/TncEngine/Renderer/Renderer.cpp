c1c6907482aba688bd2fcc21d888826116ba63d5
Ton Duc Thang <tncthng347@gmail.com>
2023-08-25 08:50:04 +0700

    renderflow and submission

--- core/src/TncEngine/Renderer/Renderer.cpp
+++ core/src/TncEngine/Renderer/Renderer.cpp
@@ -3,22 +3,29 @@
 
 namespace TncEngine {
 
-    Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData;
+    std::shared_ptr<Shader> Renderer::s_Shader = nullptr;
 
-    void Renderer::BeginScene(OrthographicCamera& camera)
+    void Renderer::BeginScene()
     {
-        s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
     }
 
     void Renderer::EndScene()
     {
     }
 
-    void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray> &vertexArray)
+    void Renderer::Bind(const std::shared_ptr<Shader> &shader)
     {
         shader->Bind();
-        shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
-        
+        s_Shader = shader;
+    }
+
+    void Renderer::Submit(const std::string &name, const glm::mat4 &matrix)
+    {
+        s_Shader->UploadUniformMat4(name, matrix);
+    }
+
+    void Renderer::Submit(const std::shared_ptr<VertexArray> &vertexArray)
+    {
         vertexArray->Bind();
         RenderCommand::DrawIndexed(vertexArray);
     }
