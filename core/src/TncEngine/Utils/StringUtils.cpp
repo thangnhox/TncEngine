f43defdb9d0659eb582772fb718ff4784da336c5
Ton Duc Thang <tncthng347@gmail.com>
2023-09-08 15:04:42 +0700

    ShaderLibrary and fix incorrect log function macro

--- core/src/TncEngine/Utils/StringUtils.cpp
+++ core/src/TncEngine/Utils/StringUtils.cpp
@@ -60,4 +60,16 @@ namespace TncEngine {
 
         return count;
     }
+
+    std::string StringUtils::PathToFileName(const std::string &filePath, bool fileExtension)
+    {
+        auto lastSlash = filePath.find_last_of("/\\");
+        lastSlash = (lastSlash == std::string::npos) ? 0 : lastSlash + 1;
+        auto lastDot = (!fileExtension) ? filePath.rfind('.') : std::string::npos;
+
+        auto count = (lastDot == std::string::npos) ? filePath.size() - lastSlash : lastDot - lastSlash;
+
+        return filePath.substr(lastSlash, count);
+    }
+
 }
--- core/src/TncEngine/Utils/StringUtils.hpp
+++ core/src/TncEngine/Utils/StringUtils.hpp
@@ -11,6 +11,7 @@ namespace TncEngine {
         static std::string LoadFile(const std::string& filepath);
         static std::unordered_map<std::string, std::string> SplitString(const std::string& keyToken, const std::string& string);
         static uint32_t CountChar(const std::string& string, char character, uint32_t begin, uint32_t end);
+        static std::string PathToFileName(const std::string& filePath, bool fileExtension = true);
     };
 
 }
